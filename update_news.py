aa1d80e540ebe3d9d0ef059338c69ec580a83938
saqibalir73-glitch <saqibalir73@gmail.com>
2026-01-30 15:38:58 +0500

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -1,31 +1,22 @@
 import json
 import datetime
 
-# Shuruat ke liye bade movies ke naam
-seed_movies = ["Pushpa", "Animal", "Singham", "Jawan", "Pathaan", "Tiger", "Stree", "Kalki", "RRR", "KGF", "Drishyam", "Dangal", "War", "Leo", "Master", "Vikram", "Batman", "Spider-Man", "Avengers", "Joker"]
-years = ["2023", "2024", "2025", "2026"]
-qualities = ["HD", "4K", "Bluray", "Web-DL"]
+# 500+ Movies Database (Sample Pattern)
+movies_list = []
+titles = ["Pushpa 2", "Singham Again", "Animal", "Stree 2", "Jawan", "Pathaan", "Tiger 3", "KGF 2", "RRR", "Salaar"]
 
-all_items = []
-
-# Loop jo 10,000 movies banayega
-for i in range(1, 10001):
-    m_name = seed_movies[i % len(seed_movies)]
-    year = years[i % len(years)]
-    quality = qualities[i % len(qualities)]
-    
-    all_items.append({
-        "title": f"{m_name} Part {i} ({year}) {quality}",
-        "img": f"https://image.tmdb.org/t/p/w500/8cdcl36U69T9PUtC77Y95Y9Y9R6m.jpg",
-        "link": f"https://www.google.com/search?q={m_name}+full+movie+download+hindi"
+for i in range(1, 501):
+    name = titles[i % len(titles)]
+    movies_list.append({
+        "title": f"{name} Part {i} (Hindi Dubbed)",
+        "img": "https://image.tmdb.org/t/p/w500/8cdcl36U69T9PUtC77Y95Y9Y9R6m.jpg",
+        "link": f"https://www.google.com/search?q={name.replace(' ', '+')}+download"
     })
 
 data = {
     "last_updated": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "news_items": all_items
+    "news_items": movies_list
 }
 
 with open('news.json', 'w') as f:
     json.dump(data, f, indent=4)
-
-print("10,000 Movies Generated Successfully!")
