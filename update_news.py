238619c5f88f3fd01fabb8a41e9505de3aaa4863
saqibalir73-glitch <saqibalir73@gmail.com>
2026-01-30 15:52:30 +0500

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -1,21 +1,19 @@
 import json
 import datetime
 
-# 500+ Movies Database (Sample Pattern)
-movies_list = []
-titles = ["Pushpa 2", "Singham Again", "Animal", "Stree 2", "Jawan", "Pathaan", "Tiger 3", "KGF 2", "RRR", "Salaar"]
-
-for i in range(1, 501):
-    name = titles[i % len(titles)]
-    movies_list.append({
-        "title": f"{name} Part {i} (Hindi Dubbed)",
-        "img": "https://image.tmdb.org/t/p/w500/8cdcl36U69T9PUtC77Y95Y9Y9R6m.jpg",
-        "link": f"https://www.google.com/search?q={name.replace(' ', '+')}+download"
-    })
+# Professional Movie Database
+movies = [
+    {"title": "Pushpa 2: The Rule (2025)", "img": "https://image.tmdb.org/t/p/w500/9v9S9r47kX98L7OUnv9Y4I3E3mS.jpg", "link": "https://www.google.com/search?q=Pushpa+2+Full+Movie+Download"},
+    {"title": "Singham Again (Hindi)", "img": "https://image.tmdb.org/t/p/w500/6v7FvN2D6mO1O5N4VfV7O9S9R6m.jpg", "link": "https://www.google.com/search?q=Singham+Again+Full+Movie+Download"},
+    {"title": "Animal (2024) Uncut", "img": "https://image.tmdb.org/t/p/w500/hr9JAz68vXmG8V6Tz8O9A5X6U5m.jpg", "link": "https://www.google.com/search?q=Animal+Uncut+Download"},
+    {"title": "Stree 2 (Hindi Dubbed)", "img": "https://image.tmdb.org/t/p/w500/7V9A5X6U5mhr9JAz68vXmG8V6Tz.jpg", "link": "https://www.google.com/search?q=Stree+2+Full+Movie+Download"},
+    {"title": "Kalki 2898 AD (4K)", "img": "https://image.tmdb.org/t/p/w500/6A5X6U5mhr9JAz68vXmG8V6Tz8O.jpg", "link": "https://www.google.com/search?q=Kalki+2898+AD+Full+Movie+Download"},
+    {"title": "Deadpool & Wolverine", "img": "https://image.tmdb.org/t/p/w500/8cdcl36U69T9PUtC77Y95Y9Y9R6m.jpg", "link": "https://www.google.com/search?q=Deadpool+Wolverine+Hindi+Download"}
+]
 
 data = {
     "last_updated": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "news_items": movies_list
+    "news_items": movies
 }
 
 with open('news.json', 'w') as f:
