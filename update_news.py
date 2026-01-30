a093aaf6a0e1aa9093694cc601e9b388bde1ffee
saqibalir73-glitch <saqibalir73@gmail.com>
2026-01-30 16:56:42 +0500

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -5,6 +5,7 @@ titles = ["Pushpa 2", "Singham Again", "Animal", "Stree 2", "Kalki 2898 AD", "Ja
 imgs = ["https://image.tmdb.org/t/p/w500/9v9S9r47kX98L7OUnv9Y4I3E3mS.jpg", "https://image.tmdb.org/t/p/w500/6v7FvN2D6mO1O5N4VfV7O9S9R6m.jpg", "https://image.tmdb.org/t/p/w500/hr9JAz68vXmG8V6Tz8O9A5X6U5m.jpg"]
 
 movies = []
+# Yeh loop 10,000 movies banayega
 for i in range(1, 10001):
     m = titles[i % len(titles)]
     movies.append({
