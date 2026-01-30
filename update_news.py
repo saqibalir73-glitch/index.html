978ad18624f0094267d5d7b296a7566524918f4d
saqibalir73-glitch <saqibalir73@gmail.com>
2026-01-30 16:47:43 +0500

    Update update_news.py

--- update_news.py
+++ update_news.py
@@ -1,20 +1,18 @@
 import json
 import datetime
 
-# Professional Movie List
-movie_pool = [
-    {"title": "Pushpa 2: The Rule", "img": "https://image.tmdb.org/t/p/w500/9v9S9r47kX98L7OUnv9Y4I3E3mS.jpg", "link": "https://www.google.com/search?q=Pushpa+2+Movie+Download"},
-    {"title": "Singham Again", "img": "https://image.tmdb.org/t/p/w500/6v7FvN2D6mO1O5N4VfV7O9S9R6m.jpg", "link": "https://www.google.com/search?q=Singham+Again+Movie+Download"},
-    {"title": "Animal (2024)", "img": "https://image.tmdb.org/t/p/w500/hr9JAz68vXmG8V6Tz8O9A5X6U5m.jpg", "link": "https://www.google.com/search?q=Animal+Movie+Download"},
-    {"title": "Stree 2", "img": "https://image.tmdb.org/t/p/w500/7V9A5X6U5mhr9JAz68vXmG8V6Tz.jpg", "link": "https://www.google.com/search?q=Stree+2+Movie+Download"},
-    {"title": "Kalki 2898 AD", "img": "https://image.tmdb.org/t/p/w500/6A5X6U5mhr9JAz68vXmG8V6Tz8O.jpg", "link": "https://www.google.com/search?q=Kalki+2898+AD+Movie+Download"}
-]
+titles = ["Pushpa 2", "Singham Again", "Animal", "Stree 2", "Kalki 2898 AD", "Jawan", "Pathaan", "Tiger 3", "Gadar 2", "Salaar"]
+imgs = ["https://image.tmdb.org/t/p/w500/9v9S9r47kX98L7OUnv9Y4I3E3mS.jpg", "https://image.tmdb.org/t/p/w500/6v7FvN2D6mO1O5N4VfV7O9S9R6m.jpg", "https://image.tmdb.org/t/p/w500/hr9JAz68vXmG8V6Tz8O9A5X6U5m.jpg"]
 
-# Robot data likh raha hai
-data = {
-    "last_updated": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "news_items": movie_pool
-}
+movies = []
+for i in range(1, 10001):
+    m = titles[i % len(titles)]
+    movies.append({
+        "title": f"{m} (2026) Hindi Dubbed Full Movie HD",
+        "img": imgs[i % len(imgs)],
+        "link": f"/download-server-{i}.html"
+    })
 
+data = {"last_updated": datetime.datetime.now().strftime("%Y-%m-%d"), "news_items": movies}
 with open('news.json', 'w') as f:
     json.dump(data, f, indent=4)
